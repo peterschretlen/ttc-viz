13bb94c3a0b20f6484cc9726aea49baf6b239f37
pschretlen <peter.schretlen@gmail.com>
2017-05-07 14:32:29 -0400

    display route bounding box

--- src/app/index.js
+++ src/app/index.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const xml2js = require('xml2js');
 const xmlParser = (new xml2js.Parser()).parseString;
 const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+const turf = require('@turf/turf');
 
 const url = 'http://webservices.nextbus.com/service/publicXMLFeed';
 const routeParams = {
@@ -10,21 +11,6 @@ const routeParams = {
     a: 'ttc',
     r: '501', 
 }
-
-axios.get(url, { params : routeParams } )
-  .then(function (response) {
-
-    xmlParser( response.data,  (e,r) => {
-
-        console.log( JSON.stringify(r.body.route));
-
-    } )
-
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-
 
 mapboxgl.accessToken = 'pk.eyJ1IjoicGV0ZXJzY2hyZXRsZW4iLCJhIjoiY2oyZHIxZ2diMDZrZjJ3cXl1bDVpY3FwZyJ9.D1guBUz1ULS2LBCltPeYOg';
 
@@ -38,3 +24,46 @@ const map = new mapboxgl.Map({
   zoom: 11,
   scrollZoom: true
 });
+
+axios.get(url, { params : routeParams } )
+  .then(function (response) {
+
+    xmlParser( response.data,  (e,r) => {
+
+        //console.log( JSON.stringify(r.body.route));
+        const rdef = r.body.route[0].$
+
+        //display a simple bouding box of the route
+        const bbox = turf.polygon([[
+            [rdef.lonMin,rdef.latMin],
+            [rdef.lonMin,rdef.latMax],
+            [rdef.lonMax,rdef.latMax],
+            [rdef.lonMax,rdef.latMin],
+            [rdef.lonMin,rdef.latMin]            
+            ]], rdef );
+
+        var route = {
+            type: 'FeatureCollection',
+            features: [ bbox ]
+        }
+
+        map.addLayer({ 
+            'id' : rdef.title, 
+            'type': 'fill',
+            'source': {
+                'type':'geojson',
+                'data': route
+            },
+            'layout': {},
+            'paint': {
+                'fill-color': '#088',
+                'fill-opacity': 0.2
+            }            
+        });
+
+    });
+
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
